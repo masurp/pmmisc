4479286af1e7cec7d8a3587c9a35b47f20bb25d8
Philipp Masur <philippmasur@Philipps-MBP.fritz.box>
2019-03-11 20:26:39 +0100

    Functions now also returns column sums, documentation is also updated.

--- R/count_na.R
+++ R/count_na.R
@@ -1,22 +1,24 @@
-#' Count overall amount of missing values
+#' Amount of missing values in a data set
 #' 
-#' This function counts the overall amount of missing values in a data set and returns a printable table. 
+#' This function produces a table with the overall amount of missing values in a data set (absolute and relative values). 
 #' 
 #' @param data A data frame.
 #' @param percent A logical value indicating whether to compute also the percentage (defaults to TRUE).
-#' 
-#' @return A tibble..
+#' @param col_sum A logical value indicating whether the column sums should be returned. 
+#' @return A tibble.
 #' @examples
 #' d <- mtcars
 #' d[4,3] <- NA # Create missing values to illustrate function
 #' 
 #' count_na(d, percent = FALSE)
-#' count_na(d)
+#' count_na(d, col_sum = FALSE)
 #' @export
-count_na <- function(data, percent = TRUE) {
+count_na <- function(data, 
+                     percent = TRUE,
+                     col_sum = TRUE) {
   
   # dependencies
-  library(dplyr)
+  library(tidyverse)
   library(magrittr)
   
   # number of missings
@@ -38,15 +40,32 @@ count_na <- function(data, percent = TRUE) {
     temp <- n %>%
       set_colnames(c("missings", "n"))
     
-    return(temp)
+    if (isTRUE(col_sum)) {
+      temp <- temp %>% 
+        bind_rows(temp %>% 
+                    summarize(n = sum(n)) %>%
+                    mutate(missings = "sum") %>%
+                    select(missings, n))
+    }
     
   } else {
     temp <- left_join(n, perc) %>%
       set_colnames(c("missings", "n", "percent"))
     
-    return(temp)
+    if (isTRUE(col_sum)) {
+      temp <- temp %>% 
+        bind_rows(temp %>% 
+                    summarize(n = sum(n),
+                              percent = sum(percent)) %>%
+                    mutate(missings = "sum") %>%
+                    select(missings, n, percent))
+    }
     
   }
+  
+  
+  
+  return(temp)
 }
 
 
