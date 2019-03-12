0815a87b690d60252bd1fe96c5b93c3d101de100
Philipp Masur <philippmasur@Philipps-MacBook-Pro.local>
2019-03-12 11:53:38 +0100

    Included print functionality.

--- R/sample_sizes.r
+++ R/sample_sizes.r
@@ -4,10 +4,12 @@
 #' 
 #' @param ... Several numeric values representing sample sizes (e.g., obtained by using \code{\link[pmmisc]{save_n}}).
 #' @param sample Names for the rows in the final table.
+#' @param digits Number of digits that should be print (only in combination with \code{print = TRUE}).
+#' @param print Logical value indicating whether the output should be formatted to fit APA-guidelines (transform varibales to character vectors).
 #' @return An object of class \code{tbl_df} that can be passed to \code{\link[papaja]{apa_table}}. It contains all sample sizes that were provided as individual numeric values.
 #' @examples
 #' # Simple example
-#' sample_sizes(100, 80, 10)
+#' sample_sizes(100, 80, 10, print = TRUE)
 #' 
 #' # Realistic example
 #' d <- mtcars
@@ -19,11 +21,19 @@
 #' n2 <- save_n(d2)
 #' 
 #' sample_sizes(n0, n1, n2)
+#' sample_sizes(n0, n1, n2, 
+#'              sample = c("Sample 1", 
+#'                         "Sample 2",
+#'                         "Sample 3"),
+#'              digits = 3,
+#'              print = TRUE)
 #' @export
 sample_sizes <- function(..., 
                          sample = c("All cases",
                                     "Eligible cases",
-                                    "Complete cases")) {
+                                    "Complete cases"),
+                         digits = 1,
+                         print = FALSE) {
   # dependencies
   library(dplyr)
   
@@ -35,6 +45,12 @@ sample_sizes <- function(...,
     as.tibble %>%
     mutate(n = as.numeric(n),
            percent = (n/unlist(list(...))[[1]])*100)
+  
+  if (isTRUE(print)) {
+    temp <- temp %>%
+      mutate(n = printnum(n, digits = 0),
+             percent = printnum(percent, digits = digits))
+  }
   return(temp)
 }
 
