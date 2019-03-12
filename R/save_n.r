2d822fb9db816bbfeee34792e01af5d98edc0ac6
Philipp Masur <philippmasur@Philipps-MacBook-Pro.local>
2019-03-12 11:57:08 +0100

    Updated the example and included link to "sample_sizes".

--- R/save_n.r
+++ R/save_n.r
@@ -1,12 +1,19 @@
 #' Save numer of cases as numeric value
 #' 
-#' Simply a convenient function to save the number of cases of a data frame. This function primarily serves to provide values for the wrapper \code{sample_sizes()} in this package.
+#' Simply a convenient function to save the number of cases of a data frame. This function primarily serves to provide values for the wrapper function \code{\link[pmmisc]{sample_sizes}}.
 #' 
 #' @param data A data frame.
 #' @return A numeric value.
 #' @examples
 #' d <- mtcars
 #' save_n(d)
+#' save_n(d[1:4,])
+#' 
+#' # In combination with the function "sample_sizes"
+#' sample_sizes(save_n(d),
+#'              save_n(d[1:4,]),
+#'              sample = c("Sample 1", "Sample 2", 
+#'              print = TRUE))
 #' @export
 save_n <- function(data) {
   as.numeric(nrow(data))
