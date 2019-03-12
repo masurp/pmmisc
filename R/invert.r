2cfc74f5e295b08bdd81b6381a5f42d7e67412aa
Philipp Masur <philippmasur@Philipps-MacBook-Pro.local>
2019-03-12 11:21:54 +0100

    Functions now supports items with any number of answer options.

--- R/invert.r
+++ R/invert.r
@@ -1,21 +1,12 @@
-#' Invert 5- or 7-point scales
+#' Invert scales with any number of answer options
 #' 
-#' This function serves as a wrapper for \code{recode} from the "car" package. It allows to easily invert 5- or 7-point scales. 
+#' This function allows to easily invert items with any number of answer options. 
 #' 
-#' @param x The variables that should be inverted
-#' @param length Is it a 5-point of 7-point scale?
-#' @return A vector
+#' @param x The variable that should be inverted (needs to be numeric!).
+#' @param length Number of answer options (defaults to )
+#' @return A vector representing the recoded variable. 
 #' @export
 invert <- function(x, 
-                   length = 5){
-  
-  # dependencies
-  if(!require(car)){install.packages('car')}
-  library(car)
-  
-  if(length == 5) {
-    car::recode(x, "1=5;2=4;3=3;4=2;5=1;NA=NA")
-  } else if(length == 7) {
-    car::recode(x, "1=7;2=6;3=5;4=4;5=3;6=2,7=1;NA=NA")
-  }
+                   length = 5) {
+  (x-(length+1))*-1
 }
